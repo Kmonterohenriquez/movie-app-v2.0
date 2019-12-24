682964c726689ade033499aa5369ebce1c4c5e8a
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-23 23:15:15 -0700

    navbar done

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import './Nav.sass'
+import {Link} from 'react-router-dom'
 
 const Nav =()=>{
     return(
         <header className="Nav">
             <div className="logo">BestMovies</div>
+            <div className="search-bar">
+                <input type="text" placeholder='Search...'/>
+                <i className="fas fa-search"></i>
+            </div>
             <nav>
                 <ul>
-                    <li><i className="fas fa-home"></i></li>
-                    <li><i className="fas fa-user-circle"></i></li>
-                    <li></li>
-                    <li></li>
+                    <Link><li><i className="fas fa-home"></i></li></Link>
+                    <Link><li><i className="fas fa-user-circle"></i></li></Link>
+                    <Link><li><i className="fas fa-search"></i></li></Link>
                 </ul>
             </nav>
         </header>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 class Section extends Component {
 	state = {};
 	render() {
@@ -9,17 +10,19 @@ class Section extends Component {
 					<div className='swiper-wrapper'>
 						{/* // loop to get all movies or show inside of the carousel */}
 						{this.props.movieData.map(curr => (
-							<div className='single-movie swiper-slide' key={curr.id}>
-								<p>{curr.title || curr.original_name}</p>
-								<img
-									src={`http://image.tmdb.org/t/p/w185/${curr.poster_path}`}
-									alt={curr.title || curr.original_name}
-								/>
-								<p className='rating'>
-									<i className='fas fa-star'></i>
-									{curr.vote_average}
-								</p>
-							</div>
+							<Link to={`/movie/${curr.id}`}key={curr.id}>
+								<div className='single-movie swiper-slide'>
+									<p>{curr.title || curr.original_name}</p>
+									<img
+										src={`http://image.tmdb.org/t/p/w185/${curr.poster_path}`}
+										alt={curr.title || curr.original_name}
+									/>
+									<p className='rating'>
+										<i className='fas fa-star'></i>
+										{curr.vote_average}
+									</p>
+								</div>
+							</Link>
 						))}
 					</div>
 				</div>
