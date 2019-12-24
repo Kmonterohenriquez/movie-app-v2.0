5575eac1259916ba089338114dd5d59e5aeb4cf4
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-23 23:32:22 -0700

    login done

---
+++ src/Pages/login.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
+
+const Login =()=> {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+         console.log('Email: ', email)
+         console.log('Password: ', password)
+    }
+		return (
+			<div className='Login'>
+				<form onSubmit={(e)=>handleSubmit(e)}>
+					<input onChange={(e)=> setEmail(e.target.value)} type='email' placeholder='Email' />
+					<input onChange={(e)=> setPassword(e.target.value)} type='password' placeholder='Password' />
+					<button type='submit'>Login</button>
+					<Link to='/signup'><button>Signup</button></Link>
+				</form>
+			</div>
+		);
+	
+}
+
+export default Login;
--- src/router.js
+++ src/router.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+// PAGES
 import Home from './Pages/Home'
+import Login from './Pages/Login'
 
 export default(
     <Switch>
-        <Route component={Home} path='/'/>
+        <Route component={Home} exact path='/'/>
+        <Route component={Login} exact path='/login'/>
     </Switch>
 )
