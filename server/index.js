95120093b909b41cf011900330a87bbe5ba02675
Kevin Montero <kevinwilding1997@gmail.com>
2020-01-06 14:36:28 -0700

    env file uploaded

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express'),
 	massive = require('massive'),
 	session = require('express-session'),
-	{ SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env,
+	{ SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env.development.local,
 	authCtrl = require('./controllers/authController'),
 	reviewCtrl = require('./controllers/reviewController'),
 	app = express();
