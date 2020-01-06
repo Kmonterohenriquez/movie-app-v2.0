5f9e0ea89d029101dfcf3b1a73faafb8ed0b77bc
Kevin Montero <kevinwilding1997@gmail.com>
2020-01-06 16:41:01 -0700

    sign up page done

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import getUser from '../../redux/actions/user/getUser';
 import profile_placeholder from '../../img/profile-placeholder.jpg';
-
+import './Register.sass';
+import userPlaceholder from '../../img/profile-placeholder.jpg';
+import lights from '../../img/lights.jpg'
 const Register = props => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
@@ -20,35 +22,64 @@ const Register = props => {
 				props.getUser(res.data);
 				props.history.push('/login');
 			})
-			.catch(err=> console.log(err.response.data.error))
-			
+			.catch(err => console.log(err.response.data.error));
 	};
-	console.log('sign up btn hit')
+	console.log('sign up btn hit');
 
 	return (
 		<div className='Register'>
-			<h1>Sign Up</h1>
-			<form onSubmit={e => handleSubmit(e)}>
-				<input
-					onChange={e => setUsername(e.target.value)}
-					type='text'
-					placeholder='Username'
-				/>
-				<input
-					onChange={e => setEmail(e.target.value)}
-					type='email'
-					placeholder='Email'
-				/>
-				<input
-					onChange={e => setPassword(e.target.value)}
-					type='password'
-					placeholder='Password'
-				/>
-				<button type='submit'>Sign up</button>
-				<Link to='/login'>
-					<button>login</button>
-				</Link>
-			</form>
+			<Link to='/login'>
+				<i
+					className='arrow fas fa-chevron-left wow bounceInLeft'
+					data-wow-delay='1s'
+				></i>
+			</Link>
+			<img
+				className='lights-img wow fadeIn'
+				data-wow-duration='1s'
+				data-wow-delay='.3s'
+				src={lights}
+				alt='lights'
+			/>
+			<div className='Register-container'>
+				<h1>Sign Up</h1>
+				<img className='profile-img' src={userPlaceholder} alt='' />
+				<form onSubmit={e => handleSubmit(e)}>
+					<div className='Username-input'>
+					<i className='fas fa-user'></i>
+						<input
+							onChange={e => setUsername(e.target.value)}
+							type='text'
+							placeholder='Username'
+						/>
+					</div>
+					<div className='Email-input'>
+					<i className="far fa-envelope"></i>
+						<input
+							onChange={e => setEmail(e.target.value)}
+							type='email'
+							placeholder='Email'
+						/>
+					</div>
+					<div className='Password-input'>
+					<i className="fas fa-lock"></i>
+						<input
+							onChange={e => setPassword(e.target.value)}
+							type='password'
+							placeholder='Password'
+						/>
+					</div>
+					<div className='btn-container'>
+						<button type='submit'>Sign up</button>
+						<p>
+							Already have an account? <br />
+							<Link to='/login'>
+								<span>Login</span>
+							</Link>
+						</p>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
