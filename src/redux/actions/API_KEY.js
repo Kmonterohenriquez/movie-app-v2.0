786364d13292440a19b0ff7596ae6233348480d0
Kevin Montero <kevinwilding1997@gmail.com>
2020-01-06 15:07:27 -0700

    avoid API KEY from being imported to github

--- src/redux/actions/API_KEY.js
+++ src/redux/actions/API_KEY.js
@@ -1,1 +1,2 @@
-export const API_KEY = 'dd36eee247f144ba66fce886e88c3fa7';
+require('dotenv').config();
+export const API_KEY = process.env.REACT_APP_MOVIEDB_API_KEY;
--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,3 +1,5 @@
+
+
 // USER
 export const GET_USER = 'GET_USER';
 export const NEW_USER = 'NEW_USER';
